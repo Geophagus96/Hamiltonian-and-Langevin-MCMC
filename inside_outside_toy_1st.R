a7137349b49c8615886e5e3c68fee257e04a2670
Yuze Zhou <33130813+Geophagus96@users.noreply.github.com>
2023-01-15 11:46:40 -0500

    Add files via upload

---
+++ inside_outside_toy_1st.R
@@ -0,0 +1,142 @@
+#f is the energy/potential function of the toy example consisting pair of variables (s1, s2)
+f = function(s){
+  B = matrix(c(0, 0.5*log(9), 0.5*log(9),0),2,2)
+  return(0.5*t(s)%*%B%*%s)
+}
+
+#grad0 is the gradient of the energy function of the toy example
+grad0 = function(s){
+  s1 = s[1]
+  s2 = s[2]
+  return(c(0.5*log(9)*s2, 0.5*log(9)*s1))
+}
+
+#inside HAMS is the function of inside HAMS, delta, eps, sigma corresponds to the three parameters, n is the number of samples
+#Negation is incorporated, but over-relaxation is not
+insideHAMS = function(delta, eps, n){
+  s1s = vector(length = n)
+  s2s = vector(length = n)
+  u1s = vector(length = n)
+  u2s = vector(length = n)
+  accs = vector(length = n)
+  s = c(1,1)
+  u = rnorm(2)
+  
+  for (i in 1:n){
+    y = s+delta*(-eps*u+sqrt(1-eps^2)*rnorm(2))
+    oldgrad = grad0(s)
+    ps = exp(oldgrad-(1/(2*delta^2))*(y-1)^2)/(exp(oldgrad-(1/(2*delta^2))*(y-1)^2)+exp(-oldgrad-(1/(2*delta^2))*(y+1)^2))
+    snew = vector(length=2)
+    for (j in 1:2){
+      if (runif(1) <= ps[j]){
+        snew[j] = 1
+      }
+      else{
+        snew[j] = -1
+      }
+    }
+    newgrad = grad0(snew)
+    unew = (eps/delta)*(y-snew)+sqrt(1-eps^2)*rnorm(2)
+    qup = prod(exp(oldgrad*(1-s)-(1/(2*delta^2))*(y-1)^2)+exp(oldgrad*(-1-s)-(1/(2*delta^2))*(y+1)^2))
+    qdown = prod(exp(newgrad*(1-snew)-(1/(2*delta^2))*(y-1)^2)+exp(newgrad*(-1-snew)-(1/(2*delta^2))*(y+1)^2))
+    log_ratio = f(snew)-f(s)+sum((oldgrad+newgrad)*(s-snew))
+    log_ratio = log_ratio-1/(2*(1-eps^2))*(sum((unew-(eps/delta)*(y-snew))^2)+sum((-u-(eps/delta)*(y-snew))^2)-sum((-u-(eps/delta)*(y-s))^2)-sum((unew-(eps/delta)*(y-s))^2))
+    q = (qup/qdown)*exp(log_ratio)
+    if (runif(1)<=q){
+      s = snew
+      u = unew
+      s1s[i] = snew[1]
+      s2s[i] = snew[2]
+      u1s[i] = unew[1]
+      u2s[i] = unew[2]
+      accs[i] = 1
+    }
+    else{
+      u = -u
+      s1s[i] = s[1]
+      s2s[i] = s[2]
+      u1s[i] = -u[1]
+      u2s[i] = -u[2]
+      accs[i] = 0
+    }
+  }
+  return(list(s1s=s1s, s2s=s2s, u1s = u1s, u2s = u2s, acc = accs))
+  #s1s: the array of samples of s1
+  #s2s: the array of samples of s2
+  #u1s: the array of samples of u1, first dimension of the momentum
+  #u2s: the array of samples of u2, second dimension of the momentum
+  #acc: the array of acceptance or rejection
+}
+
+#inside HAMS is the function of inside HAMS, delta, eps, sigma corresponds to the three parameters, n is the number of samples
+#Negation is incorporated, but over-relaxation is not
+outsideHAMS = function(delta, eps, n){
+  s1s = vector(length = n)
+  s2s = vector(length = n)
+  u1s = vector(length = n)
+  u2s = vector(length = n)
+  accs = vector(length = n)
+  s = c(1,1)
+  u = rnorm(2)
+  for (i in 1:n){
+    u12 = eps*u+sqrt(1-eps^2)*rnorm(2)
+    y = s - delta*u12
+    oldgrad = grad0(s)
+    ps = exp(oldgrad-(1/(2*delta^2))*(y-1)^2)/(exp(oldgrad-(1/(2*delta^2))*(y-1)^2)+exp(-oldgrad-(1/(2*delta^2))*(y+1)^2))
+    snew = vector(length=2)
+    for (j in 1:2){
+      if (runif(1) <= ps[j]){
+        snew[j] = 1
+      }
+      else{
+        snew[j] = -1
+      }
+    }
+    newgrad = grad0(snew)
+    unew = (y-snew)/delta
+    qup = prod(exp(oldgrad-(1/(2*delta^2))*(y-1)^2)+exp(-oldgrad-(1/(2*delta^2))*(y+1)^2))
+    qdown = prod(exp(newgrad-(1/(2*delta^2))*(y-1)^2)+exp(-newgrad-(1/(2*delta^2))*(y+1)^2))
+    log_ratio = f(snew)-f(s)+sum((oldgrad+newgrad)*(s-snew))
+    q = (qup/qdown)*exp(log_ratio)
+    
+    if (runif(1)<=q){
+      s = snew
+      u = unew
+      s1s[i] = snew[1]
+      s2s[i] = snew[2]
+      u1s[i] = unew[1]
+      u2s[i] = unew[2]
+      accs[i] = 1
+    }
+    else{
+      s1s[i] = s[1]
+      s2s[i] = s[2]
+      u1s[i] = -u12[1]
+      u2s[i] = -u12[2]
+      accs[i] = 0
+    }
+  }
+  return(list(s1s=s1s, s2s=s2s, u1s = u1s, u2s = u2s, acc = accs))
+  #s1s, s2s, u1s, u2s, accs correspond to the same variable as the inside HAMS
+}
+
+delta = 1.4
+eps = 0.9
+n = 200000
+insideresults = insideHAMS(delta, eps, n)
+
+cat('Results corresponding to the inside-HAMS algorithm:')
+cat('Estimates of p(x1=1, x2=1):', mean((insideresults$s1s==1)*(insideresults$s2s==1)))
+cat('Estimates of p(x1=1, x2=-1):', mean((insideresults$s1s==1)*(insideresults$s2s==-1)))
+cat('Estimates of p(x1=-1, x2=1):', mean((insideresults$s1s==-1)*(insideresults$s2s==1)))
+cat('Estimates of p(x1=-1, x2=-1):', mean((insideresults$s1s==-1)*(insideresults$s2s==-1)))
+cat('Acceptance probability:', mean(insideresults$acc==1))
+
+outsideresults = outsideHAMS(delta, eps, n)
+
+cat('Results corresponding to the outside-HAMS algorithm:')
+cat('Estimates of p(x1=1, x2=1):', mean((outsideresults$s1s==1)*(outsideresults$s2s==1)))
+cat('Estimates of p(x1=1, x2=-1):', mean((outsideresults$s1s==1)*(outsideresults$s2s==-1)))
+cat('Estimates of p(x1=-1, x2=1):', mean((outsideresults$s1s==-1)*(outsideresults$s2s==1)))
+cat('Estimates of p(x1=-1, x2=-1):', mean((outsideresults$s1s==-1)*(outsideresults$s2s==-1)))
+cat('Acceptance probability:', mean(outsideresults$acc==1))
